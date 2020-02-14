4f147b1bb192c04c74ef1a8ff959793fccfacf88
Horváth Martin <horvathmartin@protonmail.com>
2020-02-14 16:21:29 +0100

    Simplified HashPartitioner and readded require

--- partitioner/src/main/scala/partitioner/HashPartitioner.scala
+++ partitioner/src/main/scala/partitioner/HashPartitioner.scala
@@ -1,8 +1,8 @@
 package partitioner
 
 class HashPartitioner[T](override val numPartitions: Int) extends Partitioner[T] {
-  def getPartition(key: T): Int = {
-      val rawMod = key.hashCode % numPartitions
-      rawMod + (if (rawMod < 0) numPartitions else 0)
-  }
+  require(numPartitions > 0, s"Number of partitions ($numPartitions) should be positive.")
+
+  def getPartition(key: T): Int =
+    (key.hashCode % numPartitions + numPartitions) % numPartitions
 }
